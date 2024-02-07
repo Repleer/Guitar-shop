e2b935b92298d353a7352a02293df48383a44e43
Alex Butko <butkos89@gmail.com>
2024-02-07 18:19:04 +0200

    first commit

---
+++ All/css/script.js
@@ -0,0 +1,47 @@
+let slideIndex = 0;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function AutoSlide() {
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  slideIndex++;
+  currentSlide(slideIndex)
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex - 1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(AutoSlide, 12000);
+  
+}
